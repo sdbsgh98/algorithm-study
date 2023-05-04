17214a300ea8d28db09137c8d245553ae2b44c36
Yunho Song <131519351+sdbsgh98@users.noreply.github.com>
2023-05-04 09:30:40 +0900

    [level 0] Title: 모스부호 (1), Time: 1.98 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morseList = {".-","-...","-.-.","-..",".","..-.",
+                "--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",
+                ".--","-..-","-.--","--.."};
+        String[] morse;
+        morse = letter.split(" ");
+        for(String s : morse) {
+            for(int i = 0; i < morseList.length; i++) {
+                if(s.equals(morseList[i])) {
+                    answer += Character.toString(i + 'a');
+                }
+            }
+        }
+        return answer;
+    }
+}
