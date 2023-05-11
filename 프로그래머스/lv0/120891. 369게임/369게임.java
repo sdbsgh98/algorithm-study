64c29d2a6bc27bbeaf7d23fd8ae0dab43d258ce6
Yunho Song <131519351+sdbsgh98@users.noreply.github.com>
2023-05-11 16:06:11 +0900

    [level 0] Title: 369게임, Time: 6.12 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String strOrder = "" + order;
+        String orderStr = "" + order;
         
-        for(int i = 0; i < strOrder.length(); i++) {
-            char tempChar = strOrder.charAt(i);
+        for(int i = 0; i < orderStr.length(); i++) {
+            char tempChar = orderStr.charAt(i);
             
             if(tempChar == '3' || tempChar == '6' || tempChar == '9') {
                 answer++;
