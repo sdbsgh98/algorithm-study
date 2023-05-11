a694d7adc8481290b47cc29c74b851e8247b9ce3
Yunho Song <131519351+sdbsgh98@users.noreply.github.com>
2023-05-11 12:13:08 +0900

    [level 0] Title: 암호 해독, Time: 9.71 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        String[] s = cipher.split("");
         
-        for(int i = 0; i < s.length; i++) {
-            if((i + 1) % code == 0) {
-                answer += s[i];
-            }
+        for(int i = code - 1; i < cipher.length(); i += code) {
+            answer += cipher.charAt(i);
         }
         return answer;
     }
