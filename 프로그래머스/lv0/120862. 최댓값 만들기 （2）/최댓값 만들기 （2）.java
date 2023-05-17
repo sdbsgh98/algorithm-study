504967f71392c1cd64b781c5308c4e7f5700db12
Yunho Song <131519351+sdbsgh98@users.noreply.github.com>
2023-05-17 09:51:00 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.55 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        
+        int num1 = numbers[0] * numbers[1];
+        int num2 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        
+        if(num1 < num2) {
+            answer = num2;
+        } else {
+            answer = num1;
+        }
+        return answer;
+    }
+}
